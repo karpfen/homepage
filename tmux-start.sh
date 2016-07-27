e0aff94e108b8fdf4942bf2fab1942eee3f57b9b
Andreas Petutschnig <andreas@petutschnig.de>
2016-07-27 18:33:05 +0200

    add tmux-start.sh

---
+++ tmux-start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+SESSION="homepage"
+
+tmux -2 new-session -d -s $SESSION
+
+tmux new-window -t $SESSION:1 -k -n main 
+tmux send-keys -t $SESSION:1 'vim index.html' C-m
+tmux split-window -h
+tmux select-pane -t 1
+tmux send-keys -t $SESSION: 'vim style/style.scss' C-m
+tmux split-window -v
+tmux attach -t $SESSION
